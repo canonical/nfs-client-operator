bbb0c8e622dc4f0ce9646a8c207266fb6c20fd59
NucciTheBoss <jason.nucciarone@canonical.com>
2023-04-06 16:40:54 -0400

    feat: Add integration tests

---
+++ tests/integration/conftest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Configure integration test run."""
+
+import pathlib
+from typing import Any, Coroutine
+
+import pytest
+from _pytest.config.argparsing import Parser
+from pytest_operator.plugin import OpsTest
+
+
+def pytest_addoption(parser: Parser) -> None:
+    parser.addoption(
+        "--charm-base", action="store", default="ubuntu@22.04", help="Charm base to test."
+    )
+
+
+@pytest.fixture(scope="module")
+def charm_base(request) -> str:
+    """Get nfs-client charm series to use."""
+    return request.config.getoption("--charm-base")
+
+
+@pytest.fixture(scope="module")
+async def nfs_client_charm(ops_test: OpsTest, request) -> Coroutine[Any, Any, pathlib.Path]:
+    """Build nfs-client charm to use for integration tests."""
+    charm = await ops_test.build_charm(".")
+    return charm
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,33 +2,80 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+"""Test nfs-client charm against other NFS share provides in latest/edge."""
+
 import asyncio
 import logging
-from pathlib import Path
+import pathlib
+from typing import Any, Coroutine
 
 import pytest
-import yaml
+import tenacity
+from helpers import bootstrap_nfs_server, modify_default_profile
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-APP_NAME = METADATA["name"]
+BASE = "ubuntu"
+NFS_CLIENT = "nfs-client"
+NFS_SERVER_PROXY = "nfs-server-proxy"
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
-    """Build the charm-under-test and deploy it together with related charms.
-
-    Assert on the unit status before any relations/configurations take place.
-    """
-    # Build and deploy charm from local source folder
-    charm = await ops_test.build_charm(".")
-
-    # Deploy the charm and wait for active/idle status
+@pytest.mark.skip_if_deployed
+@pytest.mark.order(1)
+async def test_build_and_deploy(
+    ops_test: OpsTest, nfs_client_charm: Coroutine[Any, Any, pathlib.Path], charm_base: str
+) -> None:
+    """Test that nfs-client can stabilize against nfs-server-proxy."""
+    modify_default_profile()
+    endpoint = bootstrap_nfs_server()
+    logger.info(f"Deploying {NFS_CLIENT} against {NFS_SERVER_PROXY} and {BASE}")
     await asyncio.gather(
-        ops_test.model.deploy(charm, application_name=APP_NAME),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
+        ops_test.model.deploy(
+            str(await nfs_client_charm),
+            application_name=NFS_CLIENT,
+            config={"mountpoint": "/data"},
+            num_units=None,
+            base=charm_base,
+        ),
+        ops_test.model.deploy(
+            BASE,
+            application_name=BASE,
+            channel="edge",
+            num_units=1,
+            base=charm_base,
+        ),
+        ops_test.model.deploy(
+            NFS_SERVER_PROXY,
+            application_name=NFS_SERVER_PROXY,
+            config={"endpoint": endpoint},
+            channel="edge",
+            num_units=1,
+            base="ubuntu@22.04",
         ),
     )
+    # Set integrations for charmed applications
+    await ops_test.model.integrate(f"{NFS_CLIENT}:juju-info", f"{BASE}:juju-info")
+    await ops_test.model.integrate(f"{NFS_CLIENT}:nfs-share", f"{NFS_SERVER_PROXY}:nfs-share")
+    # Reduce the update status frequency to accelerate the triggering of deferred events.
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(apps=[NFS_CLIENT], status="active", timeout=1000)
+        assert ops_test.model.applications[NFS_CLIENT].units[0].workload_status == "active"
+
+
+@pytest.mark.abort_on_fail
+@pytest.mark.order(2)
+@tenacity.retry(
+    wait=tenacity.wait.wait_exponential(multiplier=2, min=1, max=30),
+    stop=tenacity.stop_after_attempt(3),
+    reraise=True,
+)
+async def test_share_active(ops_test: OpsTest) -> None:
+    """Test that NFS share is successfully mounted on principle base charm."""
+    logger.info(f"Checking that /data is mounted on principle charm {BASE}")
+    base_unit = ops_test.model.applications[BASE].units[0]
+    result = (await base_unit.ssh("ls /data")).strip("\n")
+    assert "test-1" in result
+    assert "test-2" in result
+    assert "test-3" in result
