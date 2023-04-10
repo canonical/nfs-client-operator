2357c84e857cc5952881b7c69775eebd61f0344d
NucciTheBoss <jason.nucciarone@canonical.com>
2023-04-10 13:06:13 -0400

    feat: Add tests for base charm events

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,24 +3,92 @@
 # See LICENSE file for licensing details.
 
 import unittest
+from unittest.mock import PropertyMock, patch
 
 import ops.testing
+import utils.manager as nfs
 from charm import NFSClientCharm
-from ops.model import ActiveStatus
+from ops.model import BlockedStatus, MaintenanceStatus, WaitingStatus
 from ops.testing import Harness
 
 
 class TestCharm(unittest.TestCase):
-    def setUp(self):
+    """Test nfs-client charmed operator."""
+
+    def setUp(self) -> None:
         ops.testing.SIMULATE_CAN_CONNECT = True
         self.addCleanup(setattr, ops.testing, "SIMULATE_CAN_CONNECT", False)
-
         self.harness = Harness(NFSClientCharm)
         self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
 
-    def test_start(self):
-        # Simulate the charm starting
-        self.harness.begin_with_initial_hooks()
+    @patch("utils.manager.install")
+    def test_install(self, _) -> None:
+        """Test that nfs-client can successfully be installed."""
+        self.harness.charm.on.install.emit()
+        self.assertEqual(
+            self.harness.model.unit.status, MaintenanceStatus("Installing `nfs-common`")
+        )
 
-        # Ensure we set an ActiveStatus with no message
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+    @patch("utils.manager.install", side_effect=nfs.Error("Failed to install `nfs-common`"))
+    def test_install_fail(self, _) -> None:
+        """Test that nfs-client install fail handler works."""
+        self.harness.charm.on.install.emit()
+        self.assertEqual(
+            self.harness.model.unit.status, BlockedStatus("Failed to install `nfs-common`")
+        )
+
+    @patch(
+        "charm.NFSClientCharm.config", new_callable=PropertyMock(return_value={"mountpoint": None})
+    )
+    def test_config_no_mountpoint(self, _) -> None:
+        """Test config changed handler when no mountpoint is set."""
+        self.harness.charm.on.config_changed.emit()
+        self.assertEqual(self.harness.model.unit.status, BlockedStatus("No configured mountpoint"))
+
+    @patch(
+        "charm.NFSClientCharm.config",
+        new_callable=PropertyMock(return_value={"mountpoint": "/data"}),
+    )
+    def test_config_set_mountpoint(self, _) -> None:
+        """Test config changed handler when new mountpoint is available."""
+        self.harness.charm.on.config_changed.emit()
+        self.assertEqual(self.harness.model.unit.status, WaitingStatus("Waiting for NFS share"))
+
+    @patch(
+        "charm.NFSClientCharm.config",
+        new_callable=PropertyMock(
+            return_value={
+                "mountpoint": "/data",
+                "size": 100,
+                "noexec": False,
+                "nosuid": False,
+                "nodev": False,
+                "read-only": False,
+            }
+        ),
+    )
+    def test_config_already_set(self, *_) -> None:
+        """Test config changed handler after all values have been set."""
+        # Patch charm stored state.
+        self.harness.charm._stored.mountpoint = "/data"
+        self.harness.charm._stored.size = 100
+        self.harness.charm._stored.mntopts = {
+            "noexec": False,
+            "nosuid": False,
+            "nodev": False,
+            "read-only": False,
+        }
+        self.harness.charm.on.config_changed.emit()
+        self.assertEqual(self.harness.model.unit.status, WaitingStatus("Waiting for NFS share"))
+
+    @patch("utils.manager.mounted", return_value=True)
+    @patch.object(nfs, "umount")
+    @patch("utils.manager.mounts", return_value=[])
+    @patch.object(nfs, "remove")
+    def test_on_stop(self, remove, mounts, umount, mounted) -> None:
+        """Test on stop handler."""
+        self.harness.charm.on.stop.emit()
+        umount.assert_called_once()
+        remove.assert_called_once()
+        self.assertEqual(self.harness.model.unit.status, MaintenanceStatus("Shutting down..."))
